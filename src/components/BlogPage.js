f851ce3ede760f6c6d004d8bce5d0eca9af8aa4b
HarshaKy <harshaky6573@gmail.com>
2023-04-25 17:24:00 -0700

    fix blog not found error

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -4,15 +4,19 @@ const BlogPage = ({ blogs }) => {
 
     return (
         <div className="blogPage">
-            <h1>{blog.title[0]}</h1>
-            <p>{blog.date[0]}</p>
-            <p>{blog.author[0]}</p>
-            <img
-                className="summaryImage"
-                src={blog.summary[0]['image_path']}
-                alt={blog.summary[0]['text']}
-            />
-            <p>{blog.body[0]}</p>
+            {
+                blog ?
+                    <><h1>{blog.title[0]}</h1>
+                        <p>{blog.date[0]}</p>
+                        <p>{blog.author[0]}</p>
+                        <img
+                            className="summaryImage"
+                            src={blog.summary[0]['image_path']}
+                            alt={blog.summary[0]['text']} />
+                        <p>{blog.body[0]}</p>
+                    </> :
+                    <h1>Loading...</h1>
+            }
         </div>
     )
 }
