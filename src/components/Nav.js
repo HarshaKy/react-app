edeb62a03c0901a33cd030f4e3f06f059c927643
HarshaKy <harshaky6573@gmail.com>
2023-04-25 17:48:23 -0700

    ajax call

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 const Nav = () => {
+    const [catJson, setCatJson] = useState([]);
+    const cataas = 'https://cataas.com'
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('https://cataas.com/cat?json=true');
+            const data = await response.json();
+            setCatJson(data);
+        };
+
+        fetchData();
+
+        const interval = setInterval(() => {
+            fetchData();
+        }, 60000);
+
+        return () => clearInterval(interval);
+    }, []);
+    
     return (
         <nav style={navStyle}>
             <Link to="/">Home</Link>
+            <p>New Cat every 60 seconds!</p>
+            {catJson.url ? <img style={{width: '100%', height: 'auto'}} src={`${cataas}${catJson.url}`} alt="cat" /> : <p>Loading...</p>}
         </nav>
     );
 }
